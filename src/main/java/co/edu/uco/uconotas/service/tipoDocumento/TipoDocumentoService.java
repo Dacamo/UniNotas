95a7470577143eaedf291630543a3f49921c6a46
Cristian David Castro Montes <castrom1998@gmail.com>
2019-10-17 15:47:28 -0500

    Adding new TipoDocumento Controller and Service

---
+++ src/main/java/co/edu/uco/uconotas/service/tipoDocumento/TipoDocumentoService.java
@@ -0,0 +1,11 @@
+package co.edu.uco.uconotas.service.tipoDocumento;
+
+import co.edu.uco.uconotas.entity.TipoDocumentoIdentidadEntity;
+
+import java.util.List;
+
+public interface TipoDocumentoService {
+
+    List<TipoDocumentoIdentidadEntity> findAll();
+
+}
